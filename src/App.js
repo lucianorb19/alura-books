407323a5ae88abd10ef0e3df55651d090c4917bb
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-05-15 21:15:07 -0300

    Components and map function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';//IMPORTAÇÃO DO ARQUIVO App.css - DA MESMA PASTA - ESTILO
-import logo from './imagens/logo.svg'; //IMPORTAÇÃO IMAGEM logo.svg PARA VARIÁVEL logo
+import Logo from './componentes/Logo' //Logo É O COMPONENTE (FUNÇÃO JS) QUE FICA EM /componentes/index.js
+import perfil from './imagens/perfil.svg'//ÍCONE DE PERFIL NO HEADER
+import sacola from './imagens/sacola.svg'//ÍCONE DE SACOLA NO HEADER
+
+const textoOpcoes = ['CATEGORIA','FAVORITOS','MINHA ESTANTE'];//OPÇÕES PARA O HEADER
+const icones = [perfil, sacola];//ÍCONES DO HEADER
 
 function App() {//FUNÇÃO JS QUE RETORNA UM HTML
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo alura books'></img>
-          <p><strong>Alura</strong> Books</p>
-        </div>
+        <Logo></Logo>
+        <ul className='opcoes'>
+          { textoOpcoes.map((texto) => 
+            (<li className='opcao'><p>{texto}</p></li>))}
+        </ul>
+        <ul className='icones'>
+          {icones.map((icone) => (
+            <li className='icone'><img src = {icone} alt='icone do cabeçalho'></img></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
@@ -17,7 +28,43 @@ function App() {//FUNÇÃO JS QUE RETORNA UM HTML
 /*
 ANOTAÇÕES
 className - PALAVRA RESERVADA NO React PARA DEFINIÇÃO DE CLASSES
-<img src={logo}></img> - USO DA TAG img COM A IMAGEM SENDO A VARIÁVEL logo
+<Logo></Logo> - USO DO COMPONENTE Logo
+
+TODO COMPONENTE É UMA FUNÇÃO JS CUJO RETORNO É UM HTML
+
+USO DE map EM LISTAS DE TEXTOS
+ESSE TRECHO HTML
+<ul className='opcoes'>
+    <li className='opcao'><p>CATEGORIA</p></li>
+    <li className='opcao'><p>MINHA ESTANTE</p></li>
+    <li className='opcao'><p>FAVORITOS</p></li>
+</ul>
+
+PODE SER REPRESENTADO COMO
+<ul className='opcoes'>
+  { textoOpcoes.map((texto) => 
+    (<li className='opcao'><p>{texto}</p></li>))}
+</ul>
+
+CONSIDERANDO QUE EM ALGUM PONTO ANTERIOR DO CÓDIGO JÁ TENHA SIDO DECLARADA A LISTA DE CONSTANTES
+const textoOpcoes = ['CATEGORIA','FAVORITOS','MINHA ESTANTE']
+ACIMA É USADA A FUNÇÃO map QUE FAZ ALGO ESPECÍFICO PARA CADA ITEM DA LISTA textoOpcoes, NESSE CASO,
+SALVA CADA ITEM NA VARIÁVEL texto E REPRESENTA CADA ITEM NUMA TAG li
+
+
+USO DE map EM LISTA DE COMPONENTES
+<ul className='icones'>
+  {icones.map((icone)=> (
+    <li className='icone'><img src = {icone}></img></li>
+  ))}
+</ul>
+
+CONSIDERANDO AS DECLARAÇÕES ANTERIORES
+import perfil from './imagens/perfil.svg'//ÍCONE DE PERFIL NO HEADER
+import sacola from './imagens/sacola.svg'//ÍCONE DE SACOLA NO HEADER
+const icones = [perfil, sacola];//ÍCONES DO HEADER
+
 
 */
-export default App;
+
+export default App;//TODO COMPONENTE PRECISA SER EXPORTADO AO FINAL DE SEU ARQUIVO
---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,16 @@
+import './estilo.css'//IMPORTAÇÃO ARQUIVO estilo.css DA MESMA PASTA
+import logo from '../../imagens/logo.svg' //IMPORTAÇÃO IMAGEM logo.svg PARA VARIÁVEL logo
+
+function Logo() {
+    return(
+         <div className='logo'>
+            <img src={logo} alt='logo alura books' className='logo-img'></img>
+            <p><strong>Alura</strong>Books</p>
+        </div>
+    )
+}
+
+export default Logo;//TODO COMPONENTE PRECISA SER EXPORTADO AO FINAL DE SEU ARQUIVO
+/*
+<img src={logo}></img> - USO DA TAG img COM A IMAGEM SENDO A VARIÁVEL logo
+*/
